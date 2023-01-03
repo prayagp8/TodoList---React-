3040a403f5116ecdb505f39b7b3476aa30b6370a
Prayag Patel <prayagp8@gmail.com>
2023-01-03 13:02:25 +0530

    uncount hide button added

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
+
 import './App.css';
 
 import TodoList from './component/TodoList';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <TodoList/>
- 
+import React, { useState } from 'react'
+
+function App() {
+
+  const [show, setShow] = useState(true)
+  return (
+    <div className="App">
+      <button className="btn btn-primary" onClick={() => setShow(!show)}>Hide</button>
+      {show ? <TodoList /> : <div>Good Bye</div>}
 
 
-      </div>
-    );
-  }
 
 
+    </div>
+  )
 }
 
-export default App;
+export default App
+
+
+
+
